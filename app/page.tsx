59e8dc3809ac9c9f19d71dcf8bd1ae55fe1b02f4
AlpacaUniix <senowwjy@gmail.com>
2023-05-25 16:39:38 +0700

    add image

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,9 @@ export default function Home() {
     <main className="flex min-h-screen flex-row items-center p-24 bg-red-200">
       <div className="w-full items-center justify-center font-mono lg:flex">
         <h1 className="font-bold text-5xl text-teal-700">Hello World, UNIIX</h1>
+        <div>
+          <Image src="/cuteDino.svg" width={500} height={500} alt="Cute Dino" />
+        </div>
       </div>
     </main>
   );
